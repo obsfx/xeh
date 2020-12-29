2e383024113352af1ae3f8fc3c55f8deaaae347e
Ömercan Balandı <balandiomer@gmail.com>
2020-12-29 07:30:10 +0300

    fixed heap memory overflow issue for large files

--- xeh.lisp
+++ xeh.lisp
@@ -53,6 +53,22 @@
           (if (and c (> ascii 31) (< ascii 127)) c (format nil "."))))
     str-list))
 
+(defun dump-hex-row (buffer byte-pos)
+  (let* ((row (fixed-slice buffer 0 16))
+         (left (subseq row 0 8))
+         (right (subseq row 8 16)))
+    (progn
+      (dump (string-downcase (get-row-address byte-pos))) (dump "  ")
+      (dump (string-downcase (join left " "))) (dump "  ")
+      (dump (string-downcase (join right " "))) (dump "  ")
+      (dump "|")
+      (dump (join (hex-list-to-str-list buffer)))
+      (dump "|")
+      (newline))))
+
+(defparameter byte-pos 0)
+(defparameter buffer nil)
+(defparameter file-path nil)
 (defun read-file-bytes (file-path)
   (with-open-file 
       (stream 
@@ -62,38 +78,31 @@
     ;; https://stackoverflow.com/a/3814098/13615958
     ;; https://lispcookbook.github.io/cl-cookbook/iteration.html
     (if stream
-        (loop for byte = (read-byte stream nil nil)
-          while byte
-          collect (write-to-string byte :base 16)))))
-
-(defun dump-hex-row (buffer byte-pos)
-  (if (< byte-pos (length buffer))
-      (let* ((row (fixed-slice buffer byte-pos 16))
-             (left (subseq row 0 8))
-             (right (subseq row 8 16)))
         (progn
-          (dump (string-downcase (get-row-address byte-pos))) (dump "  ")
-          (dump (string-downcase (join left " "))) (dump "  ")
-          (dump (string-downcase (join right " "))) (dump "  ")
-          (dump "|")
-          (dump (join (hex-list-to-str-list (slice buffer byte-pos 16))))
-          (dump "|")
+          (loop named stream-loop 
+                for byte = (read-byte stream nil nil)
+                while byte
+                do (progn
+                     (setq buffer (append buffer (list (write-to-string byte :base 16))))
+                     (if (= (length buffer) 16)
+                         (progn
+                           (setf buffer (format-single-char-hex-values buffer))
+                           (dump-hex-row buffer byte-pos)
+                           (setf byte-pos (+ byte-pos 16))
+                           (setf buffer nil)))))
+          (if (> (length buffer) 0)
+              (progn
+                (setf buffer (format-single-char-hex-values buffer))
+                (dump-hex-row buffer byte-pos)))
+          (dump (string-downcase (get-row-address (+ byte-pos (length buffer)))))
+          (newline))
+        (progn
+          (format t "xeh: ~A: File couldn't found" file-path)
           (newline)
-          (dump-hex-row buffer (+ byte-pos 16))))
-      (progn
-        (dump (string-downcase (get-row-address (length buffer))))
-        (newline))))
+          (quit)))))
 
-(defparameter buffer nil)
-(defparameter file-path nil)
 (defun main()
     (progn
       (setf file-path (second *posix-argv*))
       (if file-path 
-          (setf buffer (read-file-bytes file-path)))
-      (if (not buffer)
-          (progn 
-            (format t "xeh: ~A: File couldn't found" file-path)
-            (quit)))
-      (setf buffer (format-single-char-hex-values buffer))
-      (dump-hex-row buffer 0)))
+          (read-file-bytes file-path))))
